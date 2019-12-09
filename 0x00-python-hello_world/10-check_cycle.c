d60e6c69d67a0e1d1077eba928ea6896a4a86cd8
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-09 18:59:38 +0000

    Updating the last exercise

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* check_cycle - This function will check if a list has a cycle
+* @list: The list to check
+* Return: This will return 0 if the list has no cycle or 1 if it has
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *temp = list;
+	while (temp)
+	{
+		temp = temp->next;
+		if (temp == list)
+		return(1);
+	}
+	return(0);
+}
