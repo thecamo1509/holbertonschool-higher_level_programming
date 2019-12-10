3c02c41a60a409506168fb580426299d9f3a0d83
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-10 13:53:10 -0500

    Updating 5th

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range (0,100):
-	if i != 99:
-		print(str(i).zfill(2), sep='', end=', ')
-	else:
-		print(str(i))
+    if i != 99:
+        print('{:2d}'.format(i), sep='', end=', ')
+    else:
+        print('{}'.format(i))
