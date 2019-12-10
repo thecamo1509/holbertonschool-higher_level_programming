42cfe95255963bb5a920f2282e9a3025ce3d21c0
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-10 14:03:18 -0500

    Updating 6th

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in range (10):
-	for j in range (10):
-		if i == j or i > j:
-			continue
-		elif i == 8 and j == 9:
-			print(i, j, sep='')
-		else:
-			print(i, j, sep = '', end=', ')
+for i in range(10):
+    for j in range(10):
+        if i == j or i > j:
+            continue
+        elif i == 8 and j == 9:
+            print('{}{}'.format(i, j), sep='')
+        else:
+            print('{}{}'.format(i, j), sep = '', end=', ')
