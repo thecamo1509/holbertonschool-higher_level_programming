162e4c7bf395ad7f517106d29b30a660f271b6ae
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-10 14:14:59 -0500

    Updating 7th

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 def islower(c):
-	num = ord(c)
-	print(num)
-	if num >= 97 and num < 123:
-		return True
-	return False
+    num = ord(c)
+    print(num)
+    if num >= 97 and num < 123:
+        return True
+    return False
