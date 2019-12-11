c0eb9fc668aa3fe6e1c976db76e6cf8ce8f9ca30
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-10 20:19:13 -0500

    Updating pep8 and adding the bin bash

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def remove_char_at(str, n):
     copy = ""
-    for i in range (0, len(str)):
+    for i in range(0, len(str)):
         if i == n:
             continue
         copy = copy + str[i]
