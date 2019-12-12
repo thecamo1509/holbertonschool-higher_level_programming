17693453d9d9b6ef136351d9bb78d32db2a7d443
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-12 14:01:39 -0500

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,6 @@ if __name__ == '__main__':
     import sys
     qty = len(sys.argv)
     ans = 0
-    if qty == 1:
-        print(0)
-    else:
-        for i in range(1, qty):
-            ans = ans + int(sys.argv[i])
+    for i in range(1, qty):
+        ans = ans + int(sys.argv[i])
     print("{}".format(ans))
