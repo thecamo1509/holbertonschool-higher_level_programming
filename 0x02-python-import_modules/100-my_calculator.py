4c399644a8b412cb09d051ab58bd660c7fe118cb
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-12 15:03:02 -0500

    Uploading my calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    qty = len(sys.argv)
+    if qty <= 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    if sys.argv[2] == "+":
+        print("{:d} {:s} {:d} = {}".format(a, op, b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{:d} {:s} {:d} = {}".format(a, op, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{:d} {:s} {:d} = {}".format(a, op, b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{:d} {:s} {:d} = {}".format(a, op, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
