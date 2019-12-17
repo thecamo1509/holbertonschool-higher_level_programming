a80cc9a6bd0727f1f2e217ebccf4f0d603df7b16
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-17 18:58:08 -0500

    Updating the 13th

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * is_palindrome - Will check if the list is palindrome
+ * @head: Head to the first node in the list
+ * Return: 1 if it is and 0 if it is not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *ti = *head;
+	listint_t *ts = *head;
+	int i = 1, j, len = 0;
+
+	while (ti)
+	{
+		len++;
+		ti = ti->next;
+	}
+	ti = *head;
+	while (i != len)
+	{
+		for (j = 1; j < len; j++)
+		{
+			ti = ti->next;
+		}
+		if (ti->n != ts->n)
+			return (0);
+		else
+		{
+			ts = ts->next;
+			if (ti == ts)
+				break;
+			ti = *head;
+			len--;
+			i++;
+		}
+	}
+	return (1);
+}
