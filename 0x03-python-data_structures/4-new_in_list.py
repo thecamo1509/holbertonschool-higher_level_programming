425122ee13bac0c6ee87b3ccf6865a5e88dddabc
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-17 01:34:52 -0500

    Uploading the 4th task

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    newlist = my_list.copy()
+    i = 0
+    list_len = len(my_list)
+    if idx < 0:
+        return(newlist)
+    if idx > list_len:
+        return(newlist)
+    newlist[idx] = element
+    return(newlist)
