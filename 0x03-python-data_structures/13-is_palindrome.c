a2e97d870ea7301c3af412ed2d8cf2ad2e7d56b8
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-17 20:41:49 -0500

    Updating function

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,20 @@
 #include "lists.h"
 /**
+ * list_len - Will check the list of the len
+ * @head: Head to the first node:
+ * Return: the lenght
+ */
+int list_len(listint_t *head)
+{	int len = 0;
+
+	while (head)
+	{
+		len++;
+		head = head->next;
+	}
+	return (len);
+}
+/**
  * is_palindrome - Will check if the list is palindrome
  * @head: Head to the first node in the list
  * Return: 1 if it is and 0 if it is not
@@ -11,12 +26,8 @@ int is_palindrome(listint_t **head)
 	int i = 1, j, len = 0;
 
 	if (*head == NULL)
-		return(1);
-	while (ti)
-	{
-		len++;
-		ti = ti->next;
-	}
+		return (1);
+	len = list_len(ti);
 	ti = *head;
 	while (i != len)
 	{
@@ -25,7 +36,9 @@ int is_palindrome(listint_t **head)
 			ti = ti->next;
 		}
 		if (ti->n != ts->n)
+		{
 			return (0);
+		}
 		else
 		{
 			ts = ts->next;
