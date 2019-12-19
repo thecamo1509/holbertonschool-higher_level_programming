f28a337e0f3ed8832e49e2b3604c0cb41ce2c55e
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-19 13:24:05 -0500

    Fixing the return

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if (set_1 & set_2):
-        newset = set_1 & set_2
-        return(newset)
+    newset = set_1 & set_2
+    return(newset)
