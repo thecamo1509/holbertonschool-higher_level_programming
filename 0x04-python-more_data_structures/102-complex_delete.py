019b9815d5c99baf4946cb1bc3e6e4fae17d1938
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-19 18:32:40 -0500

    Updating the fist advanced

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    newlist = []
+    for i in a_dictionary:
+        if a_dictionary[i] == value:
+            newlist.append(i)
+    for j in newlist:
+        del a_dictionary[j]
+    return(a_dictionary)
