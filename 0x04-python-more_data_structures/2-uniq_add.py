78f3afd78d716b9ae54be2e1e86a85a63e334a8d
Camilo Morales <camiloandres.1509@gmail.com>
2019-12-19 13:13:07 -0500

    Updating the 2nd exercise

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,6 @@
 def uniq_add(my_list=[]):
     sum = 0
     convertedToList = list(set(my_list))
-    print(convertedToList)
     for i in range(len(convertedToList)):
         sum += convertedToList[i]
     return(sum)
