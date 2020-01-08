39af9be878cd946cee4eee13a98a74b45e959570
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-08 14:15:20 -0500

    Updating the 4th

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,6 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             newlist.append(0)
         except TypeError:
             print("wrong type")
+            newlist.append(0)
         except ZeroDivisionError:
             print("division by 0")
             newlist.append(0)
