42c43f8429d6a75278921c1300a03101802c3e03
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-08 21:46:42 -0500

    Updating

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,4 +7,4 @@ def safe_function(fct, *args):
         result = fct(args[0], args[1])
         return(result)
     except Exception as mistake:
-        print("Exception: {}".format(zero), file=stderr)
+        print("Exception: {}".format(mistake), file=stderr)
