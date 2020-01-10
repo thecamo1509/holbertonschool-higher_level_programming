691f4d6355503cdb21d5ba93a990dc93100cd465
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-09 19:37:50 -0500

    Updating the conditionals of my file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,6 +20,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) < 0 or type(value[1]) < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     @size.setter
     def size(self, value):
