30876072cb61bd231e2480cf635a53073a2fdf74
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-13 17:55:31 -0500

    Updating the 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+def text_indentation(text):
+    flag = 0
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    else:
+        for c in(text):
+            if c == '.' or c == '?' or c == ':':
+                flag = 1
+                print(c, end='')
+                continue
+            if flag == 1 and c == ' ':
+                c = ''
+                print('\n')
+                flag = 0
+            else:
+                print(c, end='')
+                flag = 0
