611a068933ed97cf865e2d05368476f58bc398a4
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-21 17:21:38 -0500

    Updating

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,10 +3,10 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, encoding="utf-8") as f:
         if nb_lines > len(f.readlines()):
             f.seek(0)
-            print(f.read())
+            print(f.read(), end="")
         if nb_lines <= 0:
             f.seek(0)
-            print(f.read())
+            print(f.read(), end="")
         else:
             f.seek(0)
             for i in range(nb_lines):
