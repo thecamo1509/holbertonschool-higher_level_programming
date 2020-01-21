056bc83c96380e2e319f1d13a3a84f53249cb1f6
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-21 00:12:38 -0500

    Updating

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 class MyList(list):
-    def __init__(self):
-        list.__init__(self)
-
     def print_sorted(self):
-        newlist = self.copy()
-        print(sorted(newlist))
+        print(sorted(self))
