e19c767ceda0028ce9c6f451eea113e2ea5576f7
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-21 16:18:48 -0500

    This is the json creation

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+import json
+import sys
+save_to_json = __import__('7-save_to_json_file').save_to_json_file
+load_from_json = __import__('8-load_from_json_file').load_from_json_file
+
+
+mylist = []
+try:
+    mylist = load_from_json("add_item.json")
+    i = 1
+    for i in range(1, len(sys.argv)):
+        mylist.append(sys.argv[i])
+
+except FileNotFoundError:
+    pass
+finally:
+    save_to_json(mylist, "add_item.json")
