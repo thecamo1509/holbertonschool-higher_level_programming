0b57b2c985c95da18befdaeaaeab0aeab131aff7
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-21 17:16:26 -0500

    Fixing

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,11 +7,9 @@ load_from_json = __import__('8-load_from_json_file').load_from_json_file
 
 try:
     mylist = load_from_json("add_item.json")
-
 except FileNotFoundError:
     mylist = []
 finally:
-    save_to_json(mylist, "add_item.json")
-    i = 1
     for i in range(1, len(sys.argv)):
         mylist.append(sys.argv[i])
+    save_to_json(mylist, "add_item.json")
