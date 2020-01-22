8ed74f6ec3ef6c90b88cddb0ca924f6ace6eb725
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-21 19:23:19 -0500

    Updating the 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dictionary = {}
+        if type(attrs) == list:
+            for i in range(len(attrs)):
+                if type(attrs[i]) == str:
+                    try:
+                        dictionary[attrs[i]] = getattr(self, attrs[i])
+                    except AttributeError:
+                        continue
+            return dictionary
+        else:
+            return (self.__dict__)
+
+    def reload_from_json(self, json):
+        for k, v in (json.items()):
+            setattr(self, k, v)
