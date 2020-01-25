4894bee18fcecab84068ecbe8552cab32179a8ef
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-25 16:36:19 -0500

    Updating the static methods and classes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+import json
+
+
 class Base:
     __nb_objects = 0
 
@@ -7,3 +11,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        with open("{}.json".format(cls.__name__), "w") as f:
+            if list_objs is None:
+                return []
+            else:
+                newlist = []
+                for obj in list_objs:
+                    mydict = obj.to_dictionary()
+                    newlist.append(mydict)
+                return f.write(cls.to_json_string(newlist))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
