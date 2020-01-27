63cd81aee714c2db7c65b7e6dd6c1c147e3e82e4
Camilo Morales <camiloandres.1509@gmail.com>
2020-01-27 13:24:03 -0500

    Updating comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" Rectangle """
 from models.base import Base
 
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initializing """
         mydict = {"width": width, "height": height, "x": x, "y": y}
         self.validator(mydict)
         self.__width = width
@@ -14,45 +16,54 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ Getter """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ Setter """
         a = {"width": value}
         self.validator(a)
         self.__width = value
 
     @property
     def height(self):
+        """ Getter """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ Setter """
         a = {"height": value}
         self.validator(a)
         self.__height = value
 
     @property
     def x(self):
+        """ Getter """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ Setter """
         a = {"x": value}
         self.validator(a)
         self.__x = value
 
     @property
     def y(self):
+        """ Getter """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ Setter """
         a = {"y": value}
         self.validator(a)
         self.__y = value
 
     def validator(self, mydict):
+        """ Validator """
         for key, value in mydict.items():
             if type(value) != int:
                 raise TypeError("{} must be an integer".format(key))
@@ -62,9 +73,11 @@ class Rectangle(Base):
                 raise ValueError("{} must be >= 0".format(key))
 
     def area(self):
+        """ Getting Rectangle area """
         return self.__width * self.__height
 
     def display(self):
+        """ Print rectangle """
         x = " " * self.__x
         y = "\n" * self.__y
         r = "#" * self.__width
@@ -73,6 +86,7 @@ class Rectangle(Base):
         print(y + (sh - 1) * (x + "#" * sw + "\n") + x + r)
 
     def __str__(self):
+        """ Str """
         mid = self.id
         mw = self.__width
         mh = self.__height
@@ -82,6 +96,7 @@ class Rectangle(Base):
         return msg
 
     def update(self, *args, **kwargs):
+        """ Update values """
         mylist = ["id", "width", "height", "x", "y"]
         c = 0
         for i in args:
@@ -92,6 +107,7 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """ Dictionary representation """
         mid = self.id
         mx = self.x
         my = self.y
