a24a6e962293c28f17b6bffc019defd691eac03f
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-02 18:15:53 -0500

    Updating

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,8 @@
 -- script that shows the number of records of equal scores --
-SELECT score, COUNT(score) FROM second_table ORDER BY score DESC GROUP BY score HAVING COUNT(score) > 1;
+SELECT 
+    score, 
+    COUNT(score) as number
+FROM
+    second_table
+GROUP BY score
+HAVING COUNT(score) >= 1;
