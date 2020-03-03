1c396d3e87180bc5911a1f537a20a7589737b6cf
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-03 18:25:31 -0500

    Updating 14th

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,8 @@
 -- script that uses the DB to show genres of Dexter --
 SELECT tv_genres.name
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_show_genres.show_id = 8 
-AND tv_show_genres.genre_id = tv_genres.id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC
 ;
