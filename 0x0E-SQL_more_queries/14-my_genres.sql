9f2110ef25d1f8874ccd7629275add1a24bc7266
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-03 17:51:30 -0500

    Updating 14th

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.show_id = 8 
 AND tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_genres.name
+ORDER BY tv_genres.name ASC
 ;
