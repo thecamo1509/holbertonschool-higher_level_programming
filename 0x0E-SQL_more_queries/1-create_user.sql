60a7554a8719764585f32027d4ac1ced60e062c6
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-03 11:33:36 -0500

    Adding tasks from 0 to 5

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,2 @@
+-- script that will create an user --
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- script that creates a table --
+CREATE TABLE IF NOT EXISTS force_name (id INT,
+    name VARCHAR(256) NOT NULL
+);
