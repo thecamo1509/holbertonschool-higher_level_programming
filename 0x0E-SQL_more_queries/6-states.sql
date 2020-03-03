1d06ed581906ec39f1b8dfac0c0783038d0de083
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-03 11:40:31 -0500

    Creating a table with auto generation

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- script that creates hbtn_0d_usa DB and states table --
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+);
