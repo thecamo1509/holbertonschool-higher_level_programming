3760f4695bc5c20946fa4decea39f69ab785c64c
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-27 18:15:36 -0500

    Updating tasks

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    mydb = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    cur = mydb.cursor()
+    cur.execute("SELECT * FROM states WHERE name RLIKE '^[N]' ORDER BY id ASC")
+    states = cur.fetchall()
+    for i in states:
+        print(i)
+    cur.close()
+    mydb.close()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    mydb = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    cur = mydb.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                (argv[4],))
+    states = cur.fetchall()
+    for i in states:
+        print(i)
+    cur.close()
+    mydb.close()
