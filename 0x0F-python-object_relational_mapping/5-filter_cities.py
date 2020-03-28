97a4c9ad132c68836129b8c0d6389106d8f7fd41
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-28 10:35:13 -0500

    Fixing pep8

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     mydb = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = mydb.cursor()
-    cur.execute("SELECT cities.name FROM states, cities WHERE states.id = cities.state_id and states.name = %s ORDER BY cities.id ASC", (argv[4],))  
+    cur.execute("""SELECT cities.name FROM states, cities
+    WHERE states.id = cities.state_id and states.name = %s
+    ORDER BY cities.id ASC""", (argv[4],))
     states = cur.fetchall()
     string = ""
     for i in states:
