3b417d42313556025590b16bfcff17913f172fa5
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-28 12:26:01 -0500

    Updating 1st with LIKE BINARY

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     mydb = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = mydb.cursor()
-    cur.execute("SELECT * FROM states WHERE name RLIKE '^[N]' ORDER BY id ASC")
+    cur.execute("""SELECT * FROM states WHERE name
+    LIKE BINARY 'N%' ORDER BY id ASC""")
     states = cur.fetchall()
     for i in states:
         print(i)
