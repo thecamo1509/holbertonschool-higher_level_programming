78a72f24e73d49cd0c325b4a7eeea03ad09a56b7
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-28 13:57:28 -0500

    Fixing pep8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = sessionmaker(bind=engine)()
     result = session.query(State).order_by(State.id)
     for i in result:
