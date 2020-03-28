8402bae4e9a0123f43ba184ea00b7f5d982abef8
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-28 16:54:49 -0500

    Finding the first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+    result = session.query(State).first()
+    if result is not None:
+        print(str(result.id) + ": " + result.name)
+    else:
+        print("Nothing")
