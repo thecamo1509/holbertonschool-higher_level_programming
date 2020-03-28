95c1a195d6adc41b4b2e30c95c2d057d1643de49
Camilo Morales <camiloandres.1509@gmail.com>
2020-03-28 18:14:42 -0500

    Task to delete a State

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = sessionmaker(bind=engine)()
     result = session.query(State).order_by(State.id)
