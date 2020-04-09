27821d3f18e4870871e7eb28c61fea0923373286
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-09 14:43:11 -0500

    Updating the algorithim

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def find_peak(list_of_integers):
-    if len(list_of_integers) != 0:
+    """This function will get the peak"""
+    if len(list_of_integers) > 0:
         peak = list_of_integers[0]
-        for i in range(len(list_of_integers)):
-            if list_of_integers[i] != peak:
-                if list_of_integers[i] > list_of_integers[i - 1] \
-                   and list_of_integers[i] > list_of_integers[i + 1]:
-                    peak2 = list_of_integers[i]
-                    if peak2 > peak:
-                        peak = peak2
-        return(peak)
+        for i in list_of_integers:
+            if i > peak:
+                peak = i
+    else:
+        peak = None
+    return (peak)
