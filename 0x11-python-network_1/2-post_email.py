057004383faaabe4d57f5ff91e3bca8e688b4fa4
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-14 18:29:38 -0500

    Creating a post request with an email address

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import urllib.parse
+import urllib.request
+from sys import argv
+
+url = argv[1]
+email = argv[2]
+
+data = urllib.parse.urlencode({'email': email})
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+	html = response.read().decode('utf-8')
+	print(html)
