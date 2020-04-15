3cc37140e7e345950fa8a726f2dfe693655792ba
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-15 13:07:48 -0500

    Adding the not import option

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,12 +4,13 @@ import urllib.parse
 import urllib.request
 from sys import argv
 
-url = argv[1]
-email = argv[2]
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
 
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    html = response.read().decode('utf-8')
-    print(html)
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('utf-8')
+        print(html)
