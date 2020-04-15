de46e26a2309601ef92f087ab2d00d85da64f037
Camilo Morales <camiloandres.1509@gmail.com>
2020-04-15 13:08:51 -0500

    Updating error status

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,11 +3,12 @@
 import urllib.request
 from sys import argv
 
-url = argv[1]
-req = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(req) as response:
-        html = response.read().decode('utf-8')
-        print(html)
-except urllib.error.HTTPError as e:
-    print('Error code: {}'.format(e.code))
+if __name__ == "__main__":
+    url = argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read().decode('utf-8')
+            print(html)
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
