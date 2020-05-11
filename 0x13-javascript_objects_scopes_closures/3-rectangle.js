21a4c55265c549632101582694e713a2d2731529
Camilo Morales <camiloandres.1509@gmail.com>
2020-05-11 12:46:24 -0500

    Updating project with semistandard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print () {
-        let i = 0;
-        while (i < this.height) {
-            console.log('X'.repeat(this.width));
-            i++;
-        }
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
     }
+  }
 };
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,25 +1,26 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    rotate () {
-        this.width = [this.height, this.height = this.width][0];
-    }
+  }
 
-    double () {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  rotate () {
+    this.width = [this.height, this.height = this.width][0];
+  }
 
-    print () {
-        let i = 0;
-        while (i < this.height) {
-            console.log('X'.repeat(this.width));
-            i++;
-        }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
     }
+  }
 };
