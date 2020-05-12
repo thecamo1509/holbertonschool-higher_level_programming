1d342c9e96b0058d0ad4129c34cc782b70f29ba6
Camilo Morales <camiloandres.1509@gmail.com>
2020-05-12 15:04:38 -0500

    Updating task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,20 @@
 #!/usr/bin/node
-const Argv = process.argv.slice(2);
+const Argv = process.argv.slice(2)
 const request = require('request');
-let id = 0;
-let count = 0;
 const mydict = {};
-request(Argv[0], function (error, response, body) {
-  if (error) {
-    console.log(error);
+request.get(Argv[0], function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else {
-    body = JSON.parse(body);
-    id = body[0].userId;
-    for (let i = 0; i < body.length; i++) {
-      if (body[i].userId !== id) {
-        mydict[id.toString()] = count;
-        id = body[i].userId;
-        count = 0;
-      }
-      if (body[i].completed === true) {
-        count++;
+    for (const tasks of (JSON.parse(body))) {
+      if (tasks.completed) {
+        if (!mydict[tasks.userId]) {
+          mydict[tasks.userId] = 1;
+        } else {
+          mydict[tasks.userId]++;
+        }
       }
     }
-    mydict[id.toString()] = count;
     console.log(mydict);
   }
 });
