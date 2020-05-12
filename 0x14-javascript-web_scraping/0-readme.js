79afea051c1756c7b72d74dc77d2db0a58991c73
Camilo Morales <camiloandres.1509@gmail.com>
2020-05-12 12:18:59 -0500

    updating

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const Argv = process.argv.slice(2);
 const file = require('fs');
-file.readFile(Argv[0], 'utf-8', (err, data) => {
+file.readFile(Argv[0], 'utf-8', function (err, data) {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
